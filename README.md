5c2d828334b90ab1ac27f49f33086ab84eb00a93
Stepan Zalis <stepan.zalis@uhk.cz>
2018-11-06 23:12:52 +0200

    init bootstrap, php pages

---
+++ README.md
@@ -0,0 +1,9 @@
+# 👨‍💻 CS425 HW 2
+
+### Dynamic website - Quiz Game 
+### The task is to create a dynamic website/game with three pages:
+    * Home Page - (game)
+    * Help - (manual)
+    * Scores - (username:score)
+    
+    
---
+++ pages/help.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles/main.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <title>Quiz Game | Help</title>
+</head>
+
+<body style="height:1000px">
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+
+    <a class="navbar-brand" href="#">Quiz Game</a>
+
+    <!-- Toggler / collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" data-toggle="tab" href="help.php">Help</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="scores.php">Scores</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+
+<div class="container-fluid" style="margin-top:80px">
+
+</div>
+
+</body>
+</html>
+
+
