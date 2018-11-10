b3136d25df27019950b36a1d26666013140fa5f9
Stepan Zalis <stepan.zalis@uhk.cz>
2018-11-10 19:40:18 +0200

    favicon

--- pages/scores.php
+++ pages/scores.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="../styles/main.css">
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
